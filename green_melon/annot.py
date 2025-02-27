0d17fdd6469c15385135699b9408d1615230667b
dinjazelena <dinjazelena@gmail.com>
2025-02-27 08:36:39 +0100

    add coco to yolo func

--- green_melon/annot.py
+++ green_melon/annot.py
@@ -7,6 +7,7 @@ coordinates (<class_index> <x_center> <y_center> <width> <height>).
 
 from __future__ import annotations
 
+import json
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
@@ -66,13 +67,13 @@ def yolo_to_pascal_box(
     return int(x_min), int(y_min), int(x_max), int(y_max)
 
 
-def xml2yolo(xml: str, labels_mapping: dict[str, int], out: str = "./") -> None:
+def xml2yolo(xml_path: str, labels_mapping: dict[str, int], out: str = "./") -> None:
     """
     Convert Pascal VOC XML format to YOLO txt format.
 
     Args:
     ----
-        xml: Path to xml file to convert.
+        xml_path: Path to xml file to convert.
         labels_mapping: A dictionary mapping class names (str) to YOLO class indices (int).
         out: Directory where to write the YOLO annotation file.
              The output filename will have the same basename as the xml file but with a .txt.
@@ -84,7 +85,7 @@ def xml2yolo(xml: str, labels_mapping: dict[str, int], out: str = "./") -> None:
     """
     yolo_format = []
 
-    tree = ET.parse(xml)  # noqa: S314
+    tree = ET.parse(xml_path)  # noqa: S314
     root = tree.getroot()
 
     img_width: int = int(root.find("size").find("width").text)
@@ -112,6 +113,57 @@ def xml2yolo(xml: str, labels_mapping: dict[str, int], out: str = "./") -> None:
 
     out_path = Path(out)
     out_path.mkdir(parents=True, exist_ok=True)
-    output_file = out_path / (Path(xml).stem + ".txt")
+    output_file = out_path / (Path(xml_path).stem + ".txt")
     with output_file.open("w") as f:
         f.write("\n".join(yolo_format))
+
+
+def coco_to_yolo_box(json_path: str, labels_mapping: dict, out: str = "./") -> None:
+    """
+    Convert COCO annotations to YOLO format and save them to text files.
+
+    This function reads a COCO-format JSON file containing image and annotation data,
+    converts each annotation's bounding box into the YOLO format (normalized x_center,
+    y_center, width, height), and writes the annotations for each image into a separate
+    text file in the specified output directory. The YOLO label for each annotation is
+    determined using the provided labels_mapping dictionary.
+
+    Args:
+    ----
+        json_path (str): The file path to the input JSON file in COCO format.
+        labels_mapping (dict): A mapping from COCO category IDs to YOLO label indices.
+        out (str, optional): The output directory where YOLO-format annotation files will be saved.
+                             Defaults to "./".
+
+    """
+    with Path(json_path).open("r") as f:
+        annotations = json.load(f)
+
+    annots_per_img: dict = {}
+    for annot in annotations["annotations"]:
+        annots_per_img.setdefault(annot["image_id"], []).append(annot)
+
+    imgs_to_ids: dict[str, dict] = {img["id"]: img for img in annotations["images"]}
+
+    for img_id, img in imgs_to_ids.items():
+        img_width: int = img["width"]
+        img_height: int = img["height"]
+
+        coordinates = []
+
+        for annot in annots_per_img[img_id]:
+            x, y, bbox_width, bbox_height = annot["bbox"]
+
+            x_center: float = (x + bbox_width / 2.0) / img_width
+            y_center: float = (y + bbox_height / 2.0) / img_height
+            bbox_width /= img_width
+            bbox_height /= img_height
+
+            yolo_label = labels_mapping.get(annot["category_id"], -1)
+
+            coordinates.append(
+                f"{yolo_label} {x_center:.6f} {y_center:.6f} {bbox_width:.6f} {bbox_height:.6f}"
+            )
+        output_file: Path = Path(out) / (Path(img["file_name"]).stem + ".txt")
+        with output_file.open("w") as f:
+            f.write("\n".join(coordinates))
